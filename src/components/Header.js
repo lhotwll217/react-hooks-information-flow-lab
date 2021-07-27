88b78784b5d161c725c00e0dc93d3baa06620051
Luke Otwell <lukehenryotwell@gmail.com>
2021-07-26 23:00:05 -0400

    commit tree 2152f3f7ce2dda663ea837b57bf557e92303eadb

---
+++ src/components/Header.js
@@ -0,0 +1,13 @@
+
+
+function Header({ handleDarkModeClick, isDarkMode }) {
+
+    return (<header>
+        <h2>Shopster</h2>
+        <button onClick={handleDarkModeClick}>
+            {isDarkMode ? "Dark" : "Light"} Mode
+        </button>
+    </header>)
+}
+
+export default Header
